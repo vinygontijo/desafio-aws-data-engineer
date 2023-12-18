404186c3dcb84f4cec520c0ead4e2710492e9acb
Vinícius Lucas Morais Gontijo <vinicius.lucas.gontijo@gmail.com>
2023-12-17 21:07:33 -0300

    Fix: Permissões Júnior

--- modules/lake_formation/main.tf
+++ modules/lake_formation/main.tf
@@ -1,6 +1,7 @@
 resource "aws_lakeformation_permissions" "permissions_junior_enriched" {
   principal = aws_iam_user.user_junior.arn
-  permissions = ["SELECT"]
+  permissions = ["SELECT", "ALTER", "DROP", "INSERT"]
+  permissions_with_grant_option = ["SELECT", "ALTER", "DROP", "INSERT"]
   data_location {
     arn = "arn:aws:s3:::dados-acs-bureau/enriched/*"
   }
@@ -8,7 +9,8 @@ resource "aws_lakeformation_permissions" "permissions_junior_enriched" {
 
 resource "aws_lakeformation_permissions" "permissions_junior_curated" {
   principal = aws_iam_user.user_junior.arn
-  permissions = ["SELECT"]
+  permissions = ["SELECT", "INSERT"]
+  permissions_with_grant_option = ["SELECT", "INSERT"]
   data_location {
     arn = "arn:aws:s3:::dados-acs-bureau/curated/*"
   }
@@ -17,6 +19,7 @@ resource "aws_lakeformation_permissions" "permissions_junior_curated" {
 resource "aws_lakeformation_permissions" "permissions_develop" {
   principal = aws_iam_user.user_develop.arn
   permissions = ["ALL"]
+  permissions_with_grant_option = ["ALL"]
   data_location {
     arn = "arn:aws:s3:::dados-acs-bureau/*"
   }
